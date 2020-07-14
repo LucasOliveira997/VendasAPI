3fe9fd2d36855290a6a616a9e8988ca4a9bd4884
LucasOliveira997 <lucas.c_julien@outlook.com>
2020-07-14 11:22:06 -0300

    KPI

--- src/controllers/VendaController.js
+++ src/controllers/VendaController.js
@@ -6,14 +6,15 @@ const Venda = require('../models/Venda');
 module.exports = {
 
     async index(req, res) {
-        const { produto } = req.query;
 
-        const venda = await Venda.find({ produto: produto });
+        const marilene = await Venda.find({ produto: "Marilene" });
+        const negoney = await Venda.find({ produto: "Nego Ney" });
+        const faustao = await Venda.find({ produto: "Faustão" });
+        const irineu = await Venda.find({ produto: "Irineu" });
 
-        
-        return res.json(venda.length);
-        
-      },
+        return res.json({ "Marilene": marilene.length, "Nego Ney": negoney.length, "Faustão": faustao.length, "Irineu": irineu.length });
+
+    },
 
     async delete(req, res) {
         const { venda_id } = req.params;
@@ -21,36 +22,36 @@ module.exports = {
         return res.send(venda);
     },
 
-    
+
 
 
     async store(req, res) {
-        const{ valor, formaPagamento, createdAt, tamanho, quantidade, produto, user, cliente } = req.body;
-          
-        
+        const { valor, formaPagamento, createdAt, tamanho, quantidade, produto, user, cliente } = req.body;
+
+
         let userCpf = await User.findOne({ cpf: user });
         let clienteCpf = await Cliente.findOne({ cpf: cliente });
 
-        if(userCpf && clienteCpf){
-        
-        venda = await Venda.create({
-            user,
-            cliente,
-            valor,
-            formaPagamento,
-            createdAt,
-            tamanho,
-            quantidade,
-            produto: produto.split(',').map(produto => produto.trim()),
-        })
-        return res.json(venda);
-    }
-    if(!userCpf){
-        return res.status(400).send({ erro: 'Vendedor não encontrado'});
-    }
-    if(!clienteCpf){
-        return res.status(401).send({ erro: 'Cliente não encontrado'});
-    }
-        
+        if (userCpf && clienteCpf) {
+
+            venda = await Venda.create({
+                user,
+                cliente,
+                valor,
+                formaPagamento,
+                createdAt,
+                tamanho,
+                quantidade,
+                produto: produto.split(',').map(produto => produto.trim()),
+            })
+            return res.json(venda);
+        }
+        if (!userCpf) {
+            return res.status(400).send({ erro: 'Vendedor não encontrado' });
+        }
+        if (!clienteCpf) {
+            return res.status(401).send({ erro: 'Cliente não encontrado' });
+        }
+
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -13,22 +13,22 @@ const routes = express.Router();
 routes.post('/authenticate', AuthController.store);
 
 //Users
-routes.post('/users',  UserController.store);
+routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
 routes.delete('/users/:user_id', UserController.delete);
 
 //Vendas
-routes.post('/vendas',  VendaController.store);
+routes.post('/vendas', VendaController.store);
 routes.get('/vendas', VendaController.index);
 routes.delete('/vendas', VendaController.delete);
 
 //clientes
-routes.post('/clientes',  ClienteController.store);
+routes.post('/clientes', ClienteController.store);
 routes.get('/clientes', ClienteController.index);
 routes.delete('/clientes', ClienteController.delete);
 
 //produtos
-routes.post('/produtos',  ProdutoController.store);
+routes.post('/produtos', ProdutoController.store);
 routes.get('/produtos', ProdutoController.index);
 routes.delete('/produtos', ProdutoController.delete);
 
